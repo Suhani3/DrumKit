84b5121b73cea81b7ca6a02880b32b4b8ec1a176
Suhani3 <35562194+Suhani3@users.noreply.github.com>
2020-05-07 15:44:49 +0530

    Udemy - JS - Event Practice

---
+++ index.js
@@ -0,0 +1,85 @@
+//Sounds on click
+for (var i = 0; i < (document.querySelectorAll(".drum").length); i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+//Sounds on keys
+document.addEventListener("keydown", keyDown);
+
+//A function can be defined inside the listener as well instead of calling- ANONYMOUS FUNCTIONS as follows..
+//
+// for (var i = 0; i < (document.querySelectorAll(".drum").length); i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function(){
+//      makeSound(this.innerHTML);
+//      }//end of FUNCTION
+//    );//end of listener
+// }
+//
+// document.addEventListener("keydown", function(event){
+//    makeSound(event.key);
+//      });
+
+function handleClick() {
+  makeSound(this.innerHTML); //can store this.innerHTML in a variable and use it
+  animateButton(this.innerHTML);
+}
+
+function keyDown(event) {
+  makeSound(event.key);
+  animateButton(event.key);
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+      break;
+
+  }
+}
+
+function animateButton(selectedKey) {
+  var activeButton = document.querySelector("." + selectedKey);
+  //add class pressed defined in styles.css to every drum key pressed
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    },
+    100);
+
+}
